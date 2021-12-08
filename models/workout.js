ee17930f08f42100c5909d7a16764a9f42038f84
unknown <jasdeepsaini30@gmail.com>
2021-12-07 21:25:57 -0500

    adding model updates

--- models/workout.js
+++ models/workout.js
@@ -27,5 +27,13 @@ const workoutSchema = new Schema({
         reps: {
             type: Number
         },
-    }, ]
+    }, ],
+    day: {
+        type: Date,
+        default: Date.now,
+    },
 })
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
